7e520bd76f6e2c1bade5c0dc09195a5931134c77
Ashwin Pavan Kadha <131484545+AshwinPavanKadha@users.noreply.github.com>
2024-01-28 23:58:34 +0530

    Update largest_element_in_array.py

--- numbers_basics/largest_element_in_array.py
+++ numbers_basics/largest_element_in_array.py
@@ -7,3 +7,15 @@ for i in my_list:
     if i > largest:
         largest=i
 print(largest)
+
+
+def find_largest(arr):
+    largest = arr[0]
+    for i in my_list:
+        if i > largest:
+            largest = i
+    return largest
+
+my_list = [1,56,7,90,34,65]
+result = find_largest(my_list)
+print(f'the largest element in array {my_list} is {result}')
